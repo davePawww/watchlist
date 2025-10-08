341452f4b72a99548fd654a42da1a2bf07a70bf9
DavePaurillo <paurillo.dave@gmail.com>
2025-10-09 00:31:27 +0800

    abstracted the navItems, added activeTab

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,7 @@ import lightLogo from "@/assets/aether-light.svg";
 import { Link } from "@tanstack/react-router";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useTheme } from "@/hooks/useTheme";
+import { useState } from "react";
 
 const navItems = [
   { to: "/", label: "Dashboard" },
@@ -11,7 +12,14 @@ const navItems = [
 ];
 
 export default function Header() {
+  const [activeTab, setActiveTab] = useState("Dashboard");
   const { theme } = useTheme();
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    setActiveTab(event.currentTarget.textContent);
+  };
 
   return (
     <header className="container mx-auto flex h-12 items-center justify-between border-r border-l border-dashed p-4">
@@ -23,9 +31,14 @@ export default function Header() {
         )}
       </div>
       <div className="flex items-center gap-4">
-        <nav className="flex gap-4">
+        <nav className="flex items-center gap-8">
           {navItems.map((item) => (
-            <Link key={item.to} to={item.to}>
+            <Link
+              key={item.to}
+              to={item.to}
+              className={`${activeTab === item.label ? "scale-110 opacity-100" : "opacity-50"} transform text-sm transition-transform duration-200 hover:scale-110 hover:opacity-100`}
+              onClick={handleClick}
+            >
               {item.label}
             </Link>
           ))}
